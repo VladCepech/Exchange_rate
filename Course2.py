71be68307ff7aa8b818698ef2ad7f8cec7c58cf6
MIScreanty <redhatawka@gmail.com>
2024-10-18 11:55:18 +0300

    добавили выпадаюший список со всеми валютами

--- Course2.py
+++ Course2.py
@@ -1,10 +1,20 @@
 import requests
 import json
 from tkinter import *
+from tkinter import ttk
+
+def generation_list_rates():
+    list_currency = []
+    answer = requests.get("https://open.er-api.com/v6/latest/RUB")
+    json_info = answer.json()
+    # i in json_info["rates"].keys():
+    #    list_currency.append(i)
+    list_currency = list(json_info["rates"].keys())
+    return list_currency
 
 #функция для получения информации с entry
 def func_exchange():
-    code = e.get().upper()
+    code = combo.get().upper()
     if code:
         answer = requests.get("https://open.er-api.com/v6/latest/RUB")
         json_info = answer.json()
@@ -23,14 +33,21 @@ window = Tk()
 window.title("Курс валют")
 window.geometry("400x400")
 
-#Vvod valuti
-e = Entry(window)
-e.pack()
+#list_currency = ["RUB", "EUR", "USD", "GBP", "JPY", "CNY", "CHF", "CAD", "AUD", "NZD", "SEK", "NOK", "BRL", "INR", "KRW", "MXN"]
+sp_currency = generation_list_rates()
+
+t_m = Label(window, text="Выберите код валюты:")
+t_m.pack(pady = [10,10])
+
+combo = ttk.Combobox(window, values=sp_currency)
+combo.pack(pady = [10,10])
 
 content_l = Label(window)
-content_l.pack()
+content_l.pack(pady = [10,10])
 
 btn = Button(window, text="Получить курс рубля", command=func_exchange)
-btn.pack()
+btn.pack(pady = [10,10])
 
 window.mainloop()
+
+print(sp_currency)
